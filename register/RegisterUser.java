f3ad31a14a963b57d2677aca88710e78579dbe43
Matheus WNB <matheuswnb@WNB.wnb>
2026-01-26 02:29:02 -0300

    Add: I removed the old function for registering users and wrote new code to add a new user.

--- register/RegisterUser.java
+++ register/RegisterUser.java
@@ -8,127 +8,60 @@ import utils.Utils;
 public class RegisterUser {
     static Scanner sc = new Scanner(System.in);
 
-    static public class GerenciadorFichas{
-        private List<String> users = new ArrayList<>();
-        private List<FichaTecnica> lists = new ArrayList<>();
+    static class UsersManagement{
+        private static List<String> users = new ArrayList<>();
+        private static List<String> passwords = new ArrayList<>();
 
-        private int id = 0;
-
-        public GerenciadorFichas(){};
-
-        void mostrarUsuarios(){
-            Utils.limparTerminal();
-
-            System.out.println(Utils.TITTLE_FORMAT + "USUÁRIOS CADASTRADOS" + Utils.TITTLE_FORMAT);
-            for(int in = 0; in < id; in++){
-               System.out.println(in + " -> " + users.get(in));
-            }
+        public UsersManagement(){};
+        
+        static void setNewNameUser(String name){
+            users.add(name);
         }
 
-        public void mostrarFichaTecn(FichaTecnica obj){
-            Utils.limparTerminal();
-
-            System.out.println(
-                "Nome: " + getUser(obj.getIdName())+"\n"+
-                "Profissão: " + obj.profissao+"\n"+
-                "Empresa: " + obj.empresa+"\n"+
-                "Tempo Trabalhado: " + obj.tempoTrabalhado+" anos\n"
-            );
-
-            sc.nextLine();
+        static void setNewPasswordUser(String password){
+            passwords.add(password);
         }
 
-        public void registerFichaTecnica(FichaTecnica obj){
-            lists.add(obj);
-        }
-
-        public FichaTecnica getList(Scanner sc){
-            mostrarUsuarios();
-            int option = Utils.validarEscolha(sc, 0, id - 1);
-            return lists.get(option);
-        }
-
-        void setId(){
-            id++;
-        }
-
-        int getId(){
-            return id;
-        }
-
-        void addUser(String nameUser){
-            users.add(nameUser);
-            setId();
-        }
-
-        String getUser(int idName){
-            return users.get(idName);
-        }
     }
 
-    static public class FichaNormal{
-        private int idName;
-        private String nome;
-        private int idade;
+    static class NewUser{
+        String nome;
+        String senha;
 
-        public FichaNormal(Scanner sc, GerenciadorFichas obj){
-            cadastrarUsuario(sc, obj);
+        public NewUser(Scanner sc){
+            this.nome = setNome(sc);
+            this.senha = setSenha(sc);
+
         }
 
-        void cadastrarUsuario(Scanner sc, GerenciadorFichas obj){
-            Utils.limparTerminal();
-            int id = obj.getId();
-
-            System.out.println(Utils.TITTLE_FORMAT + "CADASTRO" + Utils.TITTLE_FORMAT);
-            System.out.print("Digite o seu nome: ");
-            this.nome = sc.nextLine();
-
-            System.out.print("Digite sua idade: ");
-            this.idade = sc.nextInt();
-            sc.nextLine();
-
-            obj.addUser(this.nome);
-            this.idName = id;
-        }   
-
-        int getIdName(){
-            return this.idName;
+        public String setNome(Scanner sc) {
+            System.out.print("Nome de usuário: ");
+            String tempNome = sc.nextLine();
+            return tempNome;
         }
+
+        public String setSenha(Scanner sc) {
+            System.out.print("Sua senha: ");
+            String tempSenha = sc.nextLine();
+            return tempSenha;
+        }
+
+        String getName(){
+            return this.nome;
+        }
+
+         String getPassword(){
+            return this.senha;
+        }
+
+
     }
-
-    static public class FichaTecnica extends FichaNormal{
-        private String profissao;
-        private String empresa;
-        private int tempoTrabalhado;
-
-        public FichaTecnica(Scanner sc, GerenciadorFichas obj){
-            super(sc, obj);
-            cadastrarFichaTecn(sc);
-        }
-
-        void cadastrarFichaTecn (Scanner sc){
-            Utils.limparTerminal();
-
-            System.out.println(Utils.TITTLE_FORMAT + "CADASTRO FICHA TÉCNICA" + Utils.TITTLE_FORMAT);
-            System.out.print("Sua profissão: ");
-            this.profissao = sc.nextLine();
-
-            System.out.print("Sua empresa onde trabalha: ");
-            this.empresa = sc.nextLine();
-
-            System.out.print("Seu tempo trabalhado em " + this.empresa+ ":");
-            this.tempoTrabalhado = sc.nextInt();
-            sc.nextLine();
-        }
-    }
-
     public static void main(String[] args) {
-        GerenciadorFichas admin = new GerenciadorFichas();
+        System.out.println(Utils.TITTLE_FORMAT + "CADASTRO DE USUÁRIO" + Utils.TITTLE_FORMAT);
+        NewUser user = new NewUser(sc);
+        UsersManagement.setNewNameUser(user.getName());
+        UsersManagement.setNewPasswordUser(user.getPassword());
 
-        FichaTecnica ficha = new FichaTecnica(sc, admin);
-        admin.registerFichaTecnica(ficha);
-        ficha = admin.getList(sc);
-        admin.mostrarFichaTecn(ficha);
         
     }
 }
