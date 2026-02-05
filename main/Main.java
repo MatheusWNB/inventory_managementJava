2c777743d332120ac423d48085575635c2053975
Matheus Brito <matheuswnb@kali.kaliwnb>
2026-02-05 19:12:58 -0300

    Structuring the program's workflow

--- main/Main.java
+++ main/Main.java
@@ -2,43 +2,97 @@ package main;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+import db.SqlInventories.ManagerInventories;
 import db.SqlUsers.ManagerUsers;
-import login_register.RegisterInventory;
 import login_register.RegisterUser.*;
 import utils.Utils;
-import login_register.Login.UserLogin;;
+import login_register.Login.UserLogin;
+import login_register.RegisterInven.NewInventory;;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String username;
+        String resposta;
+        boolean loggedIn = false;
 
-        ManagerUsers admin = new ManagerUsers();
-        NewUser user = new NewUser(sc, admin);
+        ManagerUsers adminUsers = new ManagerUsers();
+        ManagerInventories adminInventories = new ManagerInventories();
+
+        NewUser user = null;
         UserLogin login = new UserLogin();
+        NewInventory inventory = null;
+        
+        while(true){
 
+            Utils.limparTerminal();
+            System.out.println(
+                Utils.TITTLE+ " MENU INICIAL "+ Utils.TITTLE
+            );
 
-        System.out.println(
-            "Seja bem-vindo ao seu estoque pessoal."+
-            "Vamos iniciar com o seu cadastro: "
-        );
+            System.out.println(
+                "(1) Fazer Login\n"+
+                "(2) Cadastrar nova conta\n"+
+                "(3) Sair do programa"
+            );
 
-        System.out.println(Utils.TITTLE_FORMAT + "CADASTRO DE USUÁRIO" + Utils.TITTLE_FORMAT);
+            resposta = sc.nextLine();
 
-        try{
-            admin.newUser(user.getUserName(), user.getPassword());
-        }catch(SQLException e){
-            Utils.errorSql(e);
+            switch (resposta) {
+                case "1":
+                    System.out.println
+                        (Utils.TITTLE+" LOGIN "+Utils.TITTLE);
+
+                    login.login(sc, adminUsers);
+                    loggedIn = true;
+                    break;
+
+                case "2":
+                    user = new NewUser(sc, adminUsers);
+                    adminUsers.newUser(user.getUserName(), user.getPassword());
+                    break;
+
+                case "3":
+                    System.out.println
+                        (Utils.TITTLE+" ENCERRANDO O PROGRAMA "+Utils.TITTLE);
+            
+                default:
+                    break;
+            }
+
+            if(loggedIn == true){
+                break;
+            }
+
+            continue;
         }
 
-        login.login(sc, admin);
-        username = login.getUserName();
-        System.out.println(username);
+        while(true){
+            // Utils.limparTerminal();
+            
+            System.out.println(Utils.TITTLE + " MENU ESTOQUE " + Utils.TITTLE);
+            System.out.println(
+                "(1) Registrar um novo estoque\n"+
+                "(2) Visualizar e editar estoques\n"+
+                "(3) Sair do programa\n"
+            );
+            
+            resposta = sc.nextLine();
 
+            switch (resposta) {
+                case "1":
+                    inventory = new NewInventory(login.getUserName());
+                    inventory.setInfos();
+                    inventory.registerInventory(adminInventories);
+                    
+                    break;
 
-        admin.selectAll();
-
-
+                case "2":
+                    adminInventories.printInventories(login.getUserName());
+            
+                default:
+                    break;
+            }
+        }
     }
 }
