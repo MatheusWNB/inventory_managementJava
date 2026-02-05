7e4db0398b6047437f6d4c9fdfe189bec243fcf2
Matheus Brito <matheuswnb@kali.kaliwnb>
2026-02-05 19:15:27 -0300

    Add: I rewrote the code to register inventory.

---
+++ login_register/RegisterInventories.java
@@ -0,0 +1,72 @@
+package login_register;
+
+import java.util.Scanner;
+import login_register.Login.UserLogin;
+import db.SqlInventories.ManagerInventories;
+import utils.Utils;
+
+public class RegisterInven {
+    static public class NewInventory{
+        private String userName;
+        private String password;
+        private String nameInventory;
+        static Scanner scRegister = new Scanner(System.in);
+
+        public NewInventory(String userName){
+            this.userName = userName;
+        }
+
+        public void setInfos(){
+            String resposta;
+            boolean loop = true;
+
+            Utils.limparTerminal();
+            System.out.print("Digite o nome do  seu inventário: ");
+            this.nameInventory = scRegister.nextLine();
+
+            while(loop){
+                System.out.print("Deseja adicionar uma senha ao seu inventário?: ");
+                resposta = scRegister.nextLine();
+                
+                switch(resposta){
+                    case "n":
+                        this.password = null;
+                        loop = false;
+                        break;
+
+                    case "s":
+                        System.out.println("Digite a sua senha: ");
+                        this.password = scRegister.nextLine();
+                        loop = false;
+                        break;
+
+                    default: 
+                        Utils.limparTerminal();
+                        System.out.println("Digite uma opção válida!");
+                        break;
+                }
+            }
+        }
+
+        public boolean registerInventory(ManagerInventories admin){
+            boolean validate = admin.setInventory(this.userName, this.nameInventory, this.password);
+
+            if (validate == true){
+                System.out.println(
+                    Utils.TITTLE+
+                    " ESTOQUE CRIADO COM SUCESSO! "+
+                    Utils.TITTLE
+                );
+
+            } else if(validate == false){
+                System.out.println(
+                    Utils.ERROR+
+                    " NÃO FOI POSSÍVEL CRIAR SEU ESTOQUE! "+
+                    Utils.ERROR
+                );
+            }
+
+            return validate;
+        }
+    }
+}
