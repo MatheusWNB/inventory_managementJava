66df76dee085c0efe00225cb1e9a35267775fa23
Matheus Weinfurter <matheusweinfurter@gmail.com>
2026-02-11 00:47:15 -0300

    Update: Granting access to admin_inventories in the items table.

--- db/env_example/users.sql
+++ db/env_example/users.sql
@@ -6,6 +6,9 @@ CREATE USER admin_inventories WITH PASSWORD 'yourpassword';
 GRANT ALL PRIVILEGES ON inventories TO admin_inventories;
 GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public TO admin_inventories;
 
+GRANT ALL PRIVILEGES ON items TO admin_inventories;
+GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public TO manager_inventories;
+
 CREATE USER admin_inventories WITH PASSWORD 'yourpassword';
 GRANT ALL PRIVILEGES ON users TO admin_users;
 GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public TO admin_users;
