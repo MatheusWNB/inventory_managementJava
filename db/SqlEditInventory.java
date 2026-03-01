1158245f7e00d043f5e68d21a821a4cd96dd70df
Matheus Weinfurter <matheusweinfurter@gmail.com>
2026-03-01 19:38:35 -0300

    Add: Class ManagerEditInventory. Code to register the item.

--- db/SqlEditInventory.java
+++ db/SqlEditInventory.java
@@ -1,31 +1,54 @@
 package db;
 
-import java.sql.Connection;
 import db.env.SqlAdmin.AdminInventories;
+import java.sql.*;
 import utils.Utils;
 
-import java.sql.*;
-
 public class SqlEditInventory {
-    private final String URL = AdminInventories.getUrl();
-    private final String USER = AdminInventories.getUser();
-    private final String PASSWORD = AdminInventories.getPassword();
-    private Connection conn;
+    static public class ManagerEditInventory{
+        private final String URL = AdminInventories.getUrl();
+        private final String USER = AdminInventories.getUser();
+        private final String PASSWORD = AdminInventories.getPassword();
+        private Connection conn;
 
-    private PreparedStatement psInsertItem;
+        private PreparedStatement psInsertItem;
 
-    public SqlEditInventory(){
-        try{
-            this.conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            
-            this.psInsertItem = conn.prepareStatement("""
-                INSERT INTO items (id_inventory, item, amount, unit_price, total_price)
-                VALUES (?,?,?,?,?)
-            """
-            );
+        public ManagerEditInventory(){
+            try{
+                this.conn = DriverManager.getConnection(URL, USER, PASSWORD);
+                
+                this.psInsertItem = conn.prepareStatement("""
+                    INSERT INTO items (id_inventory, item, amount, unit_price, total_price)
+                    VALUES (?,?,?,?,?)
+                """
+                );
 
-        } catch(SQLException e){
-            Utils.errorSql(e);
+            } catch(SQLException e){
+                Utils.errorSql(e);
+            }
         }
+
+        public boolean regItem(long idInventory, String item, long amount,
+                                double unitPrice, double totalPrice)
+        {
+            boolean validate;
+            try{
+                psInsertItem.setLong(1, idInventory);
+                psInsertItem.setString(2, item);
+                psInsertItem.setLong(3, amount);
+                psInsertItem.setDouble(4, unitPrice);
+                psInsertItem.setDouble(5, totalPrice);
+
+                psInsertItem.executeUpdate();
+                
+                validate = true;
+
+            } catch(SQLException e){
+                Utils.errorSql(e);
+                validate = false;
+            }
+
+            return validate;
+        }   
     }
 }
